90387336b0c81ae07e9beeb9cc981e752dbd1dec
Karvendhan M <karvsmech@gmail.com>
2020-10-22 07:09:27 +0530

    Update temp.py

--- twoNumberSum/temp.py
+++ twoNumberSum/temp.py
@@ -12,9 +12,21 @@ def twoNumberSum(array, targetSum):
 
     return []
 
+def twoNumberSum_(array, targetSum):
+    # Write your code here.
+    nums = {}
+    for idx, num in enumerate(array):
+        required_num = targetSum - num
+        pop_num = array.pop(idx)
+        if required_num in array:
+            return [num, required_num]
+        array.insert(pop_num, idx)
+    return []
+
 
 array = [3, 5, -4, 8, 11, 1, -1, 6]
 targetSum = 10
 
 twoNumberSum(array, targetSum)
+twoNumberSum_(array, targetSum)
 
